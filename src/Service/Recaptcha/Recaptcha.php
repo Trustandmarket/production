4ca977e51cd3e0ea4170d308d2201926aadf35f3
Trustandmarket <contact@kbr-global.com>
2025-11-25 00:44:56 +0100

    Update Recaptcha.php

--- src/Service/Recaptcha/Recaptcha.php
+++ src/Service/Recaptcha/Recaptcha.php
@@ -43,10 +43,10 @@ class Recaptcha
     ) : void {
         // Créez le client reCAPTCHA.
         // À FAIRE : mettre en cache le code de génération du client (recommandé) ou appeler client.close() avant de quitter la méthode.
-        putenv("GOOGLE_APPLICATION_CREDENTIALS=" . __DIR__ . '/../../../trust-market/security_form.json');
+        //putenv("GOOGLE_APPLICATION_CREDENTIALS=" . __DIR__ . '/../../../trust-market/security_form.json');
         $client = new RecaptchaEnterpriseServiceClient();
         $projectName = $client->projectName($project);
-        $result = ['response' => true, 'message' => '', 'code' => 200];
+        //$result = ['response' => true, 'message' => '', 'code' => 200];
         
         // Définissez les propriétés de l'événement à suivre.
         $event = (new Event())
