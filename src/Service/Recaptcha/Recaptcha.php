91bc15d5219970b47cc180c9a9d5cb6bcd10ce25
Trustandmarket <contact@kbr-global.com>
2026-01-04 14:47:44 +0100

    Update Recaptcha.php

--- src/Service/Recaptcha/Recaptcha.php
+++ src/Service/Recaptcha/Recaptcha.php
@@ -63,7 +63,7 @@ class Recaptcha
                 
                 printf('The CreateAssessment() call failed because the token was invalid for the following reason: ');
                 printf(InvalidReason::name($response->getTokenProperties()->getInvalidReason()));
-                return;
+                return $result;
             }
 
             // Vérifiez si l'action attendue a été exécutée.
@@ -73,6 +73,7 @@ class Recaptcha
                 // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
                 printf('The score for the protection action is:');
                 printf($response->getRiskAnalysis()->getScore());
+                return $result;
             } 
             else 
             {
@@ -83,7 +84,7 @@ class Recaptcha
         {
             printf('CreateAssessment() call failed with the following error: ');
             printf($e);
-            //return $result;
+            return $result;
         }
     }
 
