a5dbbdaa12b2c13d7aa51d7f34bfac8f3a5f21f7
Trustandmarket <contact@kbr-global.com>
2026-01-04 11:34:55 +0100

    Update Recaptcha.php

--- src/Service/Recaptcha/Recaptcha.php
+++ src/Service/Recaptcha/Recaptcha.php
@@ -43,7 +43,7 @@ class Recaptcha
         // Créez le client reCAPTCHA.
         // À FAIRE : mettre en cache le code de génération du client (recommandé) ou appeler client.close() avant de quitter la méthode.
         putenv("GOOGLE_APPLICATION_CREDENTIALS=" . __DIR__ . '/../../../trust-market/security_form.json');
-        //$client = new RecaptchaEnterpriseServiceClient();
+        $client = new RecaptchaEnterpriseServiceClient();
         $projectName = $client->projectName($project);
         $result = ['response' => true, 'message' => '', 'code' => 200];
         // Définissez les propriétés de l'événement à suivre.
@@ -59,10 +59,7 @@ class Recaptcha
             $response = $client->createAssessment($projectName, $assessment);
             // Vérifiez si le jeton est valide.
             if ($response->getTokenProperties()->getValid() == false) {
-                /*throw new \Exception('Un probleme est survenu: ' .
-                    InvalidReason::name($response->getTokenProperties()->getInvalidReason()), 400);*/
-                $result = ['response' => false, 'message' => 'Un probleme est survenu: Etes vous un humain?'
-                    /*InvalidReason::name($response->getTokenProperties()->getInvalidReason())*/, 'code' => 400];
+                $result = ['response' => false, 'message' => 'Le format de cette requette est invalide', 'code' => 400]; 
                 return $result;
             }
 
@@ -71,8 +68,8 @@ class Recaptcha
                 // Obtenez le score de risques et le ou les motifs.
                 // Pour savoir comment interpréter l'évaluation, consultez les pages suivantes :
                 // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
-                /*printf('The score for the protection action is:');
-                printf($response->getRiskAnalysis()->getScore());*/
+                printf('The score for the protection action is:');
+                printf($response->getRiskAnalysis()->getScore());
             } else {
                 //throw new \Exception('Le format de cette requette est invalide', 400);
                 $result = ['response' => false, 'message' => 'Le format de cette requette est invalide', 'code' => 400];
