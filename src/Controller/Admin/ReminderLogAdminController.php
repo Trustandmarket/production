cd60aa4573159f7ec059ee690cf132a1b9a9585b
Trustandmarket <contact@kbr-global.com>
2026-03-13 22:22:51 +0100

    Voir l'historiques des relances

--- src/Controller/Admin/ReminderLogAdminController.php
+++ src/Controller/Admin/ReminderLogAdminController.php
@@ -6,14 +6,19 @@ use App\Entity\ReminderLog;
 use App\Entity\User;
 use App\Repository\ReminderLogRepository;
 use App\Repository\UserRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ReminderLogAdminController extends AbstractController
 {
     public function __construct(
         private readonly ReminderLogRepository $reminderLogRepository,
-        private readonly UserRepository $userRepository
+        private readonly UserRepository $userRepository,
+        private readonly AdminUrlGenerator $adminUrlGenerator
     ) {
     }
 
@@ -32,5 +37,18 @@ class ReminderLogAdminController extends AbstractController
         return $this->render('admin/user/Fields/latest_reminder.html.twig', [
             'label' => $label,
         ]);
+    }
+
+    #[Route('/{_locale}/admin/reminder-log/user/{id}', name: 'admin_reminder_log_user_history', requirements: ['_locale' => 'fr'])]
+    public function userHistory(string $_locale, int $id): RedirectResponse
+    {
+        $url = $this->adminUrlGenerator
+            ->setRoute('admin', ['_locale' => $_locale])
+            ->setController(ReminderLogCrudController::class)
+            ->setAction(Action::INDEX)
+            ->set('userId', $id)
+            ->generateUrl();
+
+        return $this->redirect($url);
     }
 }
