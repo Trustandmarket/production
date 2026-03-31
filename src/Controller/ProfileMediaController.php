a9d5f1327648cf8b5881110d6dade7adc296ed4a
Trustandmarket <contact@kbr-global.com>
2026-03-31 22:29:49 +0200

    Lot 2 decoupage profilecontroller

---
+++ src/Controller/ProfileMediaController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\WpPosts;
+use App\Service\ServiceManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("", requirements={"_locale": "fr"}, name="profile_")
+ */
+class ProfileMediaController extends AbstractController
+{
+    private $service_manager;
+    private $em;
+
+    public function __construct(
+        ServiceManager $service_manager,
+        EntityManagerInterface $em
+    ) {
+        $this->service_manager = $service_manager;
+        $this->em = $em;
+    }
+
+    public function trierTableau($tabeauVideos)
+    {
+        $tab = array_unique($tabeauVideos);
+        $tab = array_filter($tab);
+        return $tab;
+    }
+
+    /**
+     * Update portfolio datas
+     * @Route("/{_locale}/profil-utilisateur/portfolio", name="portfolio")
+     * @param Request $request
+     * @return Response
+     */
+    public function portfolio(Request $request)
+    {
+        $file = $request->files->get('file');
+        $u = $this->getUser();
+        $id = $this->service_manager->portfolio($file, $this->getParameter('portfolio_directory'), $u->getId());
+        $port = $this->service_manager->readUserMeta($u->getId(), 'portfolio');
+
+        if ($port && $port->getMetaValue() != '') {
+            $id = $id . ',' . $port->getMetaValue();
+        }
+        if ($id) {
+            $this->service_manager->updateUserMeta($u->getId(), 'portfolio', $id);
+        }
+        return $this->render('admin/resultat.html.twig', [
+            'result' => 1,
+        ]);
+    }
+
+    /**
+     * @Route("/{_locale}/profil-utilisateur/delete_portfolio", name="delete_portfolio")
+     * @param Request $request
+     * @return Response
+     */
+    public function deletePortfolio(Request $request)
+    {
+        $u = $this->getUser();
+        $port = $this->service_manager->readUserMeta($u->getId(), 'portfolio');
+        $id = '';
+        if ($port && $port->getMetaValue() != '') {
+            $ids = explode(',', $port->getMetaValue());
+            for ($i = 0; $i < sizeof($ids); $i++) {
+                if ($ids[$i] != $request->get('id')) {
+                    if ($id) {
+                        $id = $id . ',' . $ids[$i];
+                    } else {
+                        $id = $ids[$i];
+                    }
+                }
+            }
+        }
+
+        if ($id) {
+            $this->service_manager->updateUserMeta(
+                $u->getId(),
+                'portfolio',
+                $id
+            );
+        }
+
+        return $this->render('admin/resultat.html.twig', [
+            'result' => 1,
+        ]);
+    }
+
+    /**
+     * @Route("/{_locale}/profil-utilisateur/deleteImagesSecondaires/{id}/{postId}", name="delete_images_secondaires")
+     * @param Request $request
+     * @param $id
+     * @return Response
+     */
+    public function deleteImagesSecondaires(Request $request, $id)
+    {
+        $port = $this->service_manager->readPostMeta($request->get('postId'), 'images_annonces');
+        $idnew = [];
+        if ($port) {
+            $ids = explode(',', $port->getMetaValue());
+            for ($i = 0; $i < sizeof($ids); $i++) {
+                if ($ids[$i] == $request->get('id')) {
+                    $img = $this->em->getRepository(WpPosts::class)->findOneById($request->get('id'));
+                    $this->em->remove($img);
+                    $this->em->flush();
+                } else {
+                    $idnew[] = $ids[$i];
+                }
+            }
+            $this->service_manager->deletePostMeta($port->getMetaId());
+        }
+        $this->service_manager->createPostMeta(
+            $request->get('postId'),
+            'images_annonces',
+            implode(',', array_unique($idnew)),
+            $request->getLocale()
+        );
+
+        return $this->render('admin/resultat.html.twig', ['result' => 1]);
+    }
+
+    /**
+     * @Route("/{_locale}/profil-utilisateur/delete_video", name="delete_video")
+     * @param Request $request
+     * @return Response
+     */
+    public function deleteVideo(Request $request)
+    {
+        $u = $this->getUser();
+        $port = $this->service_manager->readUserMeta($u->getId(), 'video');
+        $vid = [];
+        if ($port && $port->getMetaValue() != '') {
+            $ids = @unserialize($port->getMetaValue());
+
+            for ($i = 0; $i < sizeof($ids); $i++) {
+                $id = $this->service_manager->getYouTubeId($ids[$i]);
+                if (trim($id) != trim($request->get('id'))) {
+                    $vid[$i] = $ids[$i];
+                }
+            }
+        }
+
+        $this->service_manager->updateUserMeta($u->getId(), 'video', @serialize(array_filter($vid)));
+        return $this->render('admin/resultat.html.twig', [
+            'result' => 1,
+        ]);
+    }
+}
