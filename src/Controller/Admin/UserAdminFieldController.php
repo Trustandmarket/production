d28a07b07996164e452d8d8cc79aac6590b8ecbd
Trustandmarket <contact@kbr-global.com>
2026-04-04 11:39:00 +0200

    Update UserAdminFieldController.php

--- src/Controller/Admin/UserAdminFieldController.php
+++ src/Controller/Admin/UserAdminFieldController.php
@@ -3,20 +3,36 @@
 namespace App\Controller\Admin;
 
 use App\Service\Admin\UserMainActivityResolver;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserAdminFieldController extends AbstractController
 {
     public function __construct(
-        private readonly UserMainActivityResolver $userMainActivityResolver
+        private readonly UserMainActivityResolver $userMainActivityResolver,
+        private readonly LoggerInterface $logger
     ) {
     }
 
     public function mainActivityLabel(int $id): Response
     {
-        return $this->render('admin/user/Fields/main_activity.html.twig', [
-            'label' => $this->userMainActivityResolver->resolveLabel($id),
-        ]);
+        try {
+            return $this->render('admin/user/Fields/main_activity.html.twig', [
+                'label' => $this->userMainActivityResolver->resolveLabel($id),
+            ]);
+        } catch (\Throwable $e) {
+            $this->logger->error('UserAdminFieldController.mainActivityLabel failed', [
+                'user_id' => $id,
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+
+            return $this->render('admin/user/Fields/main_activity.html.twig', [
+                'label' => '',
+            ]);
+        }
     }
 }
