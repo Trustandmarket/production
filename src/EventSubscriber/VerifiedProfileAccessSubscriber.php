54ab29a5d3397f3f8341b20c426d4271df3be537
Trustandmarket <contact@kbr-global.com>
2026-04-19 19:47:09 +0200

    Update VerifiedProfileAccessSubscriber.php

--- src/EventSubscriber/VerifiedProfileAccessSubscriber.php
+++ src/EventSubscriber/VerifiedProfileAccessSubscriber.php
@@ -3,12 +3,12 @@
 namespace App\EventSubscriber;
 
 use App\Entity\User;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class VerifiedProfileAccessSubscriber implements EventSubscriberInterface
 {
@@ -33,7 +33,7 @@ class VerifiedProfileAccessSubscriber implements EventSubscriberInterface
     ];
 
     public function __construct(
-        private readonly Security $security,
+        private readonly TokenStorageInterface $tokenStorage,
         private readonly UrlGeneratorInterface $urlGenerator
     ) {
     }
@@ -51,7 +51,8 @@ class VerifiedProfileAccessSubscriber implements EventSubscriberInterface
             return;
         }
 
-        $user = $this->security->getUser();
+        $token = $this->tokenStorage->getToken();
+        $user = $token ? $token->getUser() : null;
         if (!$user instanceof User || $user->isVerified()) {
             return;
         }
